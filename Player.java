ecbfbf9bed8b5cf1d362125d5a36a10404317834
Marcus-Martir <MarcusMartir@outlook.com>
2024-03-17 14:15:35 -0500

    added shop

--- Player.java
+++ Player.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Player extends Entity {
     private Scanner input;
     private int points;
+    private int permDefence;
+    private int damageMultiplier;
 
     public Player(int numberOfPowers) {
         this.entityType = "You";
@@ -11,8 +13,60 @@ public class Player extends Entity {
         this.numberOfPowers = numberOfPowers;
     }
 
+    private boolean hasEnough(int points, int cost) { // has enough money
+        if (points - cost >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void receiveDamage(int damage) {
+        super.receiveDamage(damage - permDefence);
+    }
+
     public void goToStore() {
-        System.out.println("Went to store!");
+        System.out.println("You have won 3 matches, so you get to go to the store!!");
+        System.out.println("You have " + points + " points to spend");
+        System.out.println("To buy something, type the corresponding number next to the item.");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("  (1) cost:15 -- Health Potion - Heal for 30hp       ");
+        System.out.println("  (2) cost:5  -- Shield - Gain +2 permanent defence  ");
+        System.out.println("  (3) cost:40 -- Power - 2x super power damage       ");
+        System.out.println("  Type the letter 'e' to exit the shop               ");
+        System.out.println("-----------------------------------------------------");
+        while (true) {
+            System.out.print("\nPlease Enter an option: ");
+            String option = this.input.nextLine();
+            if (option.toUpperCase().equals("E")) {
+                System.out.println("Exiting shop...");
+                break;
+            }
+            switch (option) {
+                case "1":
+                    if (hasEnough(this.points, 15)) {
+                        this.points -= 15;
+                        System.out.println("You bought a health potion and healed 30 hitpoints!");
+                        this.health += 30;
+                    } else {System.out.println("Ooops! You don't have enough points for that...");}
+                    break;
+                case "2":
+                    if (hasEnough(this.points, 5)) {
+                        this.points -= 5;
+                        System.out.println("You bought a shield and gained +2 permanent defence!");
+                        this.permDefence += 2;
+                    } else {System.out.println("Ooops! You don't have enough points for that...");}
+                    break;
+                case "3":
+                    if (hasEnough(this.points, 40)) {
+                        this.points -= 40;
+                        System.out.println("You bought more power and now your super power does double more damage!!");
+                        this.damageMultiplier += 2;
+                    } else {System.out.println("Ooops! You don't have enough points for that...");}
+                    break;
+                default:
+                    System.out.println("Ooops! You've entered an invalid option...");
+            }
+        }
     }
 
     public void startMatchWith(Opponent opponent) {
@@ -35,7 +89,11 @@ public class Player extends Entity {
             if (index == attack) {
                 System.out.println("You rolled a " + attack + ", which is the same number you rolled before!");
                 System.out.println("You will attack with your super power!");
-                attack = 70;
+                attack = 40;
+                if (this.damageMultiplier > 0) {
+                    attack *= this.damageMultiplier;
+                    System.out.println("It seems you bought a damage multiplier! So you will deal " + this.damageMultiplier + "x more damage, which comes out to " + attack + " damage!!!");
+                }
             } else {
                 System.out.println("You rolled a " + attack + ", so you will attack with that much.");
             }
@@ -46,7 +104,7 @@ public class Player extends Entity {
             System.out.println("Your opponent rolled a " + opponentRoll + ", so that will be their defense...");
             System.out.println("\nATTACK!!!\n");
             if (index == attack) {
-                System.out.println("You used your super power, " + this.superPower + " against " + opponent.getName());
+                System.out.println("You used your super power, " + this.superPower + ", against " + opponent.getName());
 
             } else {
                 System.out.println("You used " + power + " against " + opponent.getName());
@@ -63,7 +121,7 @@ public class Player extends Entity {
             int oppAttack = (int) (Math.random() * 4) + 3;
             if (oppIndex == oppAttack) {
                 System.out.println("Your opponent rolled doubles!! Which means they'll get to use their super power...");
-                oppAttack = 70;
+                oppAttack = 40;
             } else {
                 System.out.println("Your opponent rolled. And will use \""+ oppPower + "\"" + " with " + oppAttack + " attack power...");
             }
@@ -89,6 +147,8 @@ public class Player extends Entity {
     }
 
     public void setUp() {
+        this.damageMultiplier = 0;
+        this.permDefence = 0;
         System.out.print("What will be the name of your character? ");
         this.input = new Scanner(System.in);
         this.name = this.input.nextLine();
